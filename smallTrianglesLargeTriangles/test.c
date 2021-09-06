a74e512bc7db0fe87c865d249ad4bf24134b48c2
Josh Booth <jb7008@cs.ship.edu>
2021-09-06 14:36:57 -0400

    finished smallTriangesLargeTriangles

--- smallTrianglesLargeTriangles/test.c
+++ smallTrianglesLargeTriangles/test.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+//#define DEBUG
+
 struct triangle
 {
 	int a;
@@ -10,10 +12,32 @@ struct triangle
 };
 
 typedef struct triangle triangle;
+void swap(triangle* tr, int pos1, int pos2){
+    triangle temp = tr[pos1];
+    tr[pos1] = tr[pos2];
+    tr[pos2] = temp;
+}
+
+double tri_size(triangle tri){
+    double p = ((tri.a + tri.b + tri.c)/2.0);
+    return sqrt((p*(p-tri.a)*(p-tri.b)*(p-tri.c)));
+}
+
+int cmp_tris(triangle t1, triangle t2){
+    return (tri_size(t1) > tri_size(t2) ? 1 : 0);
+}
+
 void sort_by_area(triangle* tr, int n) {
-	/**
-	* Sort an array a of the length n
-	*/
+    /**
+    * Sort an array a of the length n
+    */
+    for(int i = 1; i < n; i++){
+        for(int j = i; j > 0; j--){
+            if(cmp_tris(tr[j],tr[j-1]) == 0){
+                swap(tr, j, j-1);
+            }
+        }
+    }
 }
 
 int main()
@@ -26,7 +50,11 @@ int main()
 	}
 	sort_by_area(tr, n);
 	for (int i = 0; i < n; i++) {
+		#ifdef DEBUG
+			printf("%d %d %d %f\n", tr[i].a, tr[i].b, tr[i].c, tri_size(tr[i]));
+		#else
 		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+		#endif
 	}
 	return 0;
 }
