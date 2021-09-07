5d320bc8d367f2090db4511398e53f7fe2f8cf2a
Josh Booth <jb7008@cs.ship.edu>
2021-09-06 20:45:25 -0400

    finished postTransition

--- postTransition/test.c
+++ postTransition/test.c
@@ -1,201 +1,148 @@
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
-#define MIN_ELEMENT 1
-#define MAX_ELEMENT 1000000
-int  sum (int count,...) {
-    va_list ap; // argument pointer
-    va_start(ap, count); // initialize list
-    int sum = 0;
-    for(int i = 0; i < count; i++){
-        sum+=va_arg(ap, int); // access next value from the argument pointer of type int
-    }
+#define MAX_STRING_LENGTH 6
 
-    va_end(ap);
-    return sum;
-}
-
-int min(int count,...) {
-    va_list ap;
-    va_start(ap, count);
-    int min = va_arg(ap, int); // min value is first value in list
-    int cur;
-    for(int i = 0; i < count-1; i++){
-        cur = va_arg(ap,int);
-        min = (cur < min) ? cur : min;
-    }
-    va_end(ap);
-    return min;
-}
-
-int max(int count,...) {
-    va_list ap;
-    va_start(ap, count);
-    int max = va_arg(ap, int);
-    int cur;
-    for(int i = 0; i < count-1; i++){
-        cur = va_arg(ap,int);
-        max = (cur > max) ? cur : max;
-    }
-    va_end(ap);
-    return max;
-}
-
-int test_implementations_by_sending_three_elements() {
-    srand(time(NULL));
-    
-    int elements[3];
-    
-    elements[0] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[1] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[2] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    
-    fprintf(stderr, "Sending following three elements:\n");
-    for (int i = 0; i < 3; i++) {
-        fprintf(stderr, "%d\n", elements[i]);
-    }
-    
-    int elements_sum = sum(3, elements[0], elements[1], elements[2]);
-    int minimum_element = min(3, elements[0], elements[1], elements[2]);
-    int maximum_element = max(3, elements[0], elements[1], elements[2]);
-
-    fprintf(stderr, "Your output is:\n");
-    fprintf(stderr, "Elements sum is %d\n", elements_sum);
-    fprintf(stderr, "Minimum element is %d\n", minimum_element);
-    fprintf(stderr, "Maximum element is %d\n\n", maximum_element);
-    
-    int expected_elements_sum = 0;
-    for (int i = 0; i < 3; i++) {
-        if (elements[i] < minimum_element) {
-            return 0;
-        }
-        
-        if (elements[i] > maximum_element) {
-            return 0;
-        }
-        
-        expected_elements_sum += elements[i];
-    }
-    
-    return elements_sum == expected_elements_sum;
-}
-
-int test_implementations_by_sending_five_elements() {
-    srand(time(NULL));
-    
-    int elements[5];
-    
-    elements[0] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[1] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[2] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[3] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[4] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    
-    fprintf(stderr, "Sending following five elements:\n");
-    for (int i = 0; i < 5; i++) {
-        fprintf(stderr, "%d\n", elements[i]);
-    }
-    
-    int elements_sum = sum(5, elements[0], elements[1], elements[2], elements[3], elements[4]);
-    int minimum_element = min(5, elements[0], elements[1], elements[2], elements[3], elements[4]);
-    int maximum_element = max(5, elements[0], elements[1], elements[2], elements[3], elements[4]);
-    
-    fprintf(stderr, "Your output is:\n");
-    fprintf(stderr, "Elements sum is %d\n", elements_sum);
-    fprintf(stderr, "Minimum element is %d\n", minimum_element);
-    fprintf(stderr, "Maximum element is %d\n\n", maximum_element);
-    
-    int expected_elements_sum = 0;
-    for (int i = 0; i < 5; i++) {
-        if (elements[i] < minimum_element) {
-            return 0;
-        }
-        
-        if (elements[i] > maximum_element) {
-            return 0;
-        }
-        
-        expected_elements_sum += elements[i];
-    }
-    
-    return elements_sum == expected_elements_sum;
-}
-
-int test_implementations_by_sending_ten_elements() {
-    srand(time(NULL));
-    
-    int elements[10];
-    
-    elements[0] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[1] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[2] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[3] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[4] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[5] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[6] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[7] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[8] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    elements[9] = rand() % (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT;
-    
-    fprintf(stderr, "Sending following ten elements:\n");
-    for (int i = 0; i < 10; i++) {
-        fprintf(stderr, "%d\n", elements[i]);
-    }
-    
-    int elements_sum = sum(10, elements[0], elements[1], elements[2], elements[3], elements[4],
-                           elements[5], elements[6], elements[7], elements[8], elements[9]);
-    int minimum_element = min(10, elements[0], elements[1], elements[2], elements[3], elements[4],
-                           elements[5], elements[6], elements[7], elements[8], elements[9]);
-    int maximum_element = max(10, elements[0], elements[1], elements[2], elements[3], elements[4],
-                           elements[5], elements[6], elements[7], elements[8], elements[9]);
-    
-    fprintf(stderr, "Your output is:\n");
-    fprintf(stderr, "Elements sum is %d\n", elements_sum);
-    fprintf(stderr, "Minimum element is %d\n", minimum_element);
-    fprintf(stderr, "Maximum element is %d\n\n", maximum_element);
-    
-    int expected_elements_sum = 0;
-    for (int i = 0; i < 10; i++) {
-        if (elements[i] < minimum_element) {
-            return 0;
-        }
-        
-        if (elements[i] > maximum_element) {
-            return 0;
-        }
-        
-        expected_elements_sum += elements[i];
-    }
-    
-    return elements_sum == expected_elements_sum;
-}
-
-int main ()
+struct package
 {
-    int number_of_test_cases;
-    scanf("%d", &number_of_test_cases);
-    
-    while (number_of_test_cases--) {
-        if (test_implementations_by_sending_three_elements()) {
-            printf("Correct Answer\n");
-        } else {
-            printf("Wrong Answer\n");
-        }
-        
-        if (test_implementations_by_sending_five_elements()) {
-            printf("Correct Answer\n");
-        } else {
-            printf("Wrong Answer\n");
-        }
-        
-        if (test_implementations_by_sending_ten_elements()) {
-            printf("Correct Answer\n");
-        } else {
-            printf("Wrong Answer\n");
+	char* id;
+	int weight;
+};
+
+typedef struct package package;
+
+struct post_office
+{
+	int min_weight;
+	int max_weight;
+	package* packages;
+	int packages_count;
+};
+
+typedef struct post_office post_office;
+
+struct town
+{
+	char* name;
+	post_office* offices;
+	int offices_count;
+};
+
+typedef struct town town;
+
+
+
+void print_all_packages(town t) {
+    printf("%s:\n", t.name);
+    for(int i = 0; i < t.offices_count; i++){ // for each office
+        printf("\t%d:\n",i);
+        for(int j = 0; j < t.offices[i].packages_count; j++){
+            printf("\t\t%s\n",t.offices[i].packages[j].id);
         }
     }
-    
-    return 0;
+}
+
+void send_all_acceptable_packages(town* source, int source_office_index, town* target, int target_office_index) {	
+		int source_unsent_packages_count = 0;
+		package* source_unsent_packages = malloc(sizeof(package)*1);
+	for(int i = 0; i < source->offices[source_office_index].packages_count; i++){ // for each package
+		// declare common values to reduce length of comparisons
+		package current_package = source->offices[source_office_index].packages[i];
+		int target_package_count = target->offices[target_office_index].packages_count;
+		int source_package_count = source->offices[source_office_index].packages_count;
+
+		if(current_package.weight >= target->offices[target_office_index].min_weight && // if it is in the weight range
+			current_package.weight <= target->offices[target_office_index].max_weight){
+				// add package to end of target list and increment number of packages
+				// allocate space for new package
+				target->offices[target_office_index].packages = realloc(target->offices[target_office_index].packages, sizeof(package)*(target_package_count+1));
+				memcpy(&(target->offices[target_office_index].packages[target_package_count]), &(source->offices[source_office_index].packages[i]), sizeof(package));
+				target->offices[target_office_index].packages_count = target->offices[target_office_index].packages_count+1;
+
+		} else{
+			source_unsent_packages_count++;
+			source_unsent_packages = realloc(source_unsent_packages, sizeof(package)*source_unsent_packages_count);
+			source_unsent_packages[source_unsent_packages_count-1] = source->offices[source_office_index].packages[i];
+		}
+	}
+	
+		source->offices[source_office_index].packages = source_unsent_packages;
+		source->offices[source_office_index].packages_count = source_unsent_packages_count;
+}
+
+town town_with_most_packages(town* towns, int towns_count) {
+    town max_packages = towns[0];
+    int max_count = 0;
+    for(int i = 0; i < towns_count; i++){
+        int count = 0;
+        for(int j = 0; j < towns[i].offices_count; j++){
+            count+=towns[i].offices[j].packages_count;
+        }
+        if(count > max_count){
+            max_count = count;
+            max_packages = towns[i];
+        }
+    }
+    return max_packages;
+}
+
+town* find_town(town* towns, int towns_count, char* name) {
+    for(int i = 0; i < towns_count; i++){
+        if(strcmp(towns[i].name, name) == 0){
+            return &towns[i];
+        }
+    }
+    return &towns[0];
+}
+
+int main()
+{
+	int towns_count;
+	scanf("%d", &towns_count);
+	town* towns = malloc(sizeof(town)*towns_count);
+	for (int i = 0; i < towns_count; i++) {
+		towns[i].name = malloc(sizeof(char) * MAX_STRING_LENGTH);
+		scanf("%s", towns[i].name);
+		scanf("%d", &towns[i].offices_count);
+		towns[i].offices = malloc(sizeof(post_office)*towns[i].offices_count);
+		for (int j = 0; j < towns[i].offices_count; j++) {
+			scanf("%d%d%d", &towns[i].offices[j].packages_count, &towns[i].offices[j].min_weight, &towns[i].offices[j].max_weight);
+			towns[i].offices[j].packages = malloc(sizeof(package)*towns[i].offices[j].packages_count);
+			for (int k = 0; k < towns[i].offices[j].packages_count; k++) {
+				towns[i].offices[j].packages[k].id = malloc(sizeof(char) * MAX_STRING_LENGTH);
+				scanf("%s", towns[i].offices[j].packages[k].id);
+				scanf("%d", &towns[i].offices[j].packages[k].weight);
+			}
+		}
+	}
+	int queries;
+	scanf("%d", &queries);
+	char town_name[MAX_STRING_LENGTH];
+	while (queries--) {
+		int type;
+		scanf("%d", &type);
+		switch (type) {
+		case 1:
+			scanf("%s", town_name);
+			town* t = find_town(towns, towns_count, town_name);
+			print_all_packages(*t);
+			break;
+		case 2:
+			scanf("%s", town_name);
+			town* source = find_town(towns, towns_count, town_name);
+			int source_index;
+			scanf("%d", &source_index);
+			scanf("%s", town_name);
+			town* target = find_town(towns, towns_count, town_name);
+			int target_index;
+			scanf("%d", &target_index);
+			send_all_acceptable_packages(source, source_index, target, target_index);
+			break;
+		case 3:
+			printf("Town with the most number of packages is %s\n", town_with_most_packages(towns, towns_count).name);
+			break;
+		}
+	}
+	return 0;
 }
